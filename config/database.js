0c5fca034a44b53c4a7286aa9aff6db7868a478f
YuYun95 <2219946651@qq.com>
2020-09-27 23:07:30 +0800

    frist commit

---
+++ config/database.js
@@ -0,0 +1,34 @@
+module.exports = ({env}) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'blog'),
+        password: env('DATABASE_PASSWORD', 'ofewRM452fgRD'),
+      },
+      options: {},
+    },
+  },
+});
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab8aa26d-cd55-4444-8d94-b4978b245a9a'
+};
